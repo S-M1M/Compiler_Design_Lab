86f71bfb3ab05ea139313d60c80aacc95665181d
Suraia Akter Mim <msuraia55@gmail.com>
2025-06-03 20:41:01 +0600

    C file
    C program to simulate a lexical analyzer for validating  operator

---
+++ Valid_operator.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char op[10];
+
+    printf("Enter an operator: ");
+    scanf("%s", op);
+
+    if (
+        strcmp(op, "+") == 0 || strcmp(op, "-") == 0 ||
+        strcmp(op, "++") == 0 || strcmp(op, "--") == 0 ||
+        strcmp(op, "*") == 0 || strcmp(op, "/") == 0 ||
+        strcmp(op, "%") == 0 || strcmp(op, "%=") == 0 ||
+        strcmp(op, "==") == 0 || strcmp(op, "<") == 0 ||
+        strcmp(op, ">") == 0 || strcmp(op, "<=") == 0 ||
+        strcmp(op, ">=") == 0 || strcmp(op, "&&") == 0 ||
+        strcmp(op, "||") == 0 || strcmp(op, "!") == 0 ||
+        strcmp(op, "&") == 0 || strcmp(op, "|") == 0 ||
+        strcmp(op, "^") == 0 || strcmp(op, "~") == 0 ||
+        strcmp(op, "<<") == 0 || strcmp(op, ">>") == 0 ||
+        strcmp(op, "=") == 0 || strcmp(op, "+=") == 0 ||
+        strcmp(op, "-=") == 0 || strcmp(op, "*=") == 0 ||
+        strcmp(op, "/=") == 0 || strcmp(op, "%=") == 0 ||
+        strcmp(op, "&=") == 0 || strcmp(op, "|=") == 0 ||
+        strcmp(op, "^=") == 0
+    ) {
+        printf("Valid operator.\n");
+    } else {
+        printf("Invalid operator.\n");
+    }
+
+    return 0;
+}
